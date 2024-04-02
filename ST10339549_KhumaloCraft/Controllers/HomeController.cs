f7491b8c743cc8d9ca06fa5b51745e6b827a0be8
Melissa Govender <melissa.govender@derivco.co.za>
2024-04-02 18:56:46 +0200

    updates

--- ST10339549_KhumaloCraft/Controllers/HomeController.cs
+++ ST10339549_KhumaloCraft/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace ST10339549_KhumaloCraft.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
